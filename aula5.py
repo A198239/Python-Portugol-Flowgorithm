101108ead72407ea0afe8e613eb7471a7a1c9382
A198239 <77178419+A198239@users.noreply.github.com>
2021-01-18 15:00:33 -0300

    Add files via upload

---
+++ aula5.py
@@ -0,0 +1,29 @@
+
+# lista = [10, 2, 8, 6]
+# tupla = (2, 4, 6, 8)
+# tupla_lista = tuple(lista)
+# print(type(tupla_lista))
+# print(tupla_lista)
+# lista_numerica = list(tupla)
+# print(lista_numerica)
+
+# lista.sort()
+# print(lista)
+# lista.reverse()
+# print(lista)
+
+# for x in lista_produtos:
+#     print(x)
+
+# lista = [2, 4, 6, 8]
+# print(max(lista))
+
+# if 'carne seca' in lista_produtos:
+#     print('existe carne seca na lista.')
+# else:
+#     print('não há carne seca na lista. Será incluído.')
+#     lista_produtos.append('carne seca')
+#     print(lista_produtos)
+# lista_produtos.pop(4)
+# print(lista_produtos)
+
