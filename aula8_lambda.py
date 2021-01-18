b856bb4bb8a196c93340b5eac6a5debf1f1e4a4e
A198239 <77178419+A198239@users.noreply.github.com>
2021-01-18 15:02:35 -0300

    Add files via upload

---
+++ aula8_lambda.py
@@ -0,0 +1,7 @@
+# contador_letras = lambda lista: [len(x) for x in lista]
+#
+# lista_nomes = ['anderson', 'luciana', 'ana lucia', 'emilia']
+# print(contador_letras(lista_nomes))
+#
+# soma = lambda a, b: a + b
+# print(soma(35, 77))
